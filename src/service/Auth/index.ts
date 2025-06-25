4cb4cb00f43c36373cf8d598ba32b5f85022bb2f
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-06-25 17:46:06 +0600

    add authentication functions: implement Register and Login with API integration

---
+++ src/service/Auth/index.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const Register = async (userdata: any) => {
+  try {
+    const res = await fetch("http://localhost:3000/api/auth/register", {
+      method: "POSOT",
+      headers: {
+        "CONTENT-TYPE": "application/json",
+      },
+      body: JSON.stringify(userdata),
+    });
+
+    const result = await res.json();
+    if (result.success) {
+      (await cookies()).set("accessToken", result.data.accessToken);
+    }
+    return result;
+  } catch (error) {
+    console.error("Error during registration:", error);
+    throw new Error("Registration failed");
+  }
+};
+
+export const Login = async (userdata: any) => {
+  try {
+    const res = await fetch("", {
+      method: "POST",
+      headers: {
+        "CONTENT-TYPE": "application/json",
+      },
+      body: JSON.stringify(userdata),
+    });
+    const result = await res.json();
+    if (result.success) {
+      (await cookies()).set("accessToken", result.data.accessToken);
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw new Error("Login failed");
+  }
+};
