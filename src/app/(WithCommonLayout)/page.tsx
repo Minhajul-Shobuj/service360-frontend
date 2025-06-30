a39b53a37ab30df35ad8ecd30ea21be258635796
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-06-30 14:17:32 +0600

    added cloudinary

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,9 +1,9 @@
-import ExpertTeamSection from "@/Component/Home/ExpertSection";
-import HeroSection from "@/Component/Home/HeroSection";
-import ServicesSection from "@/Component/Home/ServiceSection";
-import TestimonialSection from "@/Component/Home/TestimonialSection";
-import Footer from "@/Component/Shared/Footer";
-import Navbar from "@/Component/Shared/Navbar";
+import ExpertTeamSection from "@/component/Home/ExpertSection";
+import HeroSection from "@/component/Home/HeroSection";
+import ServicesSection from "@/component/Home/ServiceSection";
+import TestimonialSection from "@/component/Home/TestimonialSection";
+import Footer from "@/component/Shared/Footer";
+import Navbar from "@/component/Shared/Navbar";
 
 export default function Home() {
   return (
---
+++ src/app/(WithCommonLayout)/services/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import ImageUploader from "@/ui/imageUploader";
+
+const Services = () => {
+  return (
+    <>
+      <div className="bg-red-100 h-screen flex items-center justify-center">
+        <ImageUploader />
+      </div>
+    </>
+  );
+};
+
+export default Services;
