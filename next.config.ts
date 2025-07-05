16c096444d945d322fb8cb5ef3f5afcda0aa9042
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-07-05 16:09:34 +0600

    feat: add new sections for Best For You, Franchise, How It Works, Trending, and Why Choose Us; update Home page layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "unsplash.com",
+      "images.unsplash.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
 };
 
 export default nextConfig;
